88fbb103f6c54a99117124d8695ea92067841a16
Nathaniel Kinzelberg <nathanielkinzelberg@gmail.com>
2026-09-23 15:35:10 +0300

    Refactored Locking Controller aka Servo motor

--- firmware/src/LockController.cpp
+++ firmware/src/LockController.cpp
@@ -1,1 +1,19 @@
 #include "LockController.h"
+#include <Arduino.h>
+#include <ESP32Servo.h>
+
+static Servo myServo;
+
+void lockInit() {
+  pinMode(4, INPUT_PULLUP);
+  myServo.attach(14);
+  myServo.write(0);
+}
+
+void lockClose() {
+  myServo.write(0);
+}
+
+void lockOpen() {
+    myServo.write(100);
+}
--- firmware/src/LockController.h
+++ firmware/src/LockController.h
@@ -1,1 +1,10 @@
 #pragma once
+
+/* Initializes the lock */
+void lockInit();
+
+/* Opens the lock */
+void lockOpen();
+
+/* Closes the lock */
+void lockClose();
