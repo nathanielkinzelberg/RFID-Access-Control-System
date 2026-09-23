ae128ee3f197373df0685d41d65e1542058f24c0
Nathaniel Kinzelberg <nathanielkinzelberg@gmail.com>
2026-09-23 15:18:47 +0300

    Refactored Buzzer to isolated file

--- firmware/src/Buzzer.cpp
+++ firmware/src/Buzzer.cpp
@@ -1,1 +1,33 @@
 #include "Buzzer.h"
+#include <Arduino.h>
+
+void buzzerInit() {
+  pinMode(13, OUTPUT);
+}
+
+
+void beepAccessGranted() {
+  digitalWrite(13, HIGH);
+  delay(2000);
+  digitalWrite(13, LOW);
+}
+
+void beepAccessDenied() {
+  for(int i = 0; i < 5; i++){
+    digitalWrite(13, HIGH);
+    delay(250);
+    digitalWrite(13, LOW);
+    delay(250);
+  }
+}
+
+void beepLock() {
+    for(int i = 0; i < 7; i++){
+    digitalWrite(13, HIGH);
+    delay(50);
+    digitalWrite(13, LOW);
+    delay(50);
+  }
+}
+
+
--- firmware/src/Buzzer.h
+++ firmware/src/Buzzer.h
@@ -1,1 +1,13 @@
 #pragma once
+
+/* Initializes the buzzer */
+void buzzerInit();
+
+/* One long beep*/
+void beepAccessGranted();
+
+/*5 short beeps*/
+void beepAccessDenied();
+
+/*7 super-short beeps*/
+void beepLock();
